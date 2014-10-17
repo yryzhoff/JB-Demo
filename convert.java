3a7f2bd9862d252644ddb47e15802574a37542e4
HerbertBinkley <Herbert.Binkley@gmail.com>
2014-10-17 16:45:56 +0400

    Update java converter

--- convert.java
+++ convert.java
@@ -1,51 +1,41 @@
 public class IntToText {
 
-    private static final String[] SCALES = {"", "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion"};
-    private static final String[] SUBTWENTY = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
-                                               "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
-    private static final String[] DECADES = {"zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+   private static final String[] SCALES = {"", "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion"};
+private static final String[] SUBTWENTY = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+                                           "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+private static final String[] DECADES = {"", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
 
-    /**
-     * Convert any value from 0 to 999 inclusive, to a string.
-     * @param value The value to convert.
-     * @param and whether to use the word 'and' in the output.
-     * @return a String representation of the value.
-     */
-    private static final String tripleAsText(int value, boolean and) {
-        if (value < 0 || value >= 1000) {
-            throw new IllegalArgumentException("Illegal triple-value " + value);
-        }
+/**
+ * Convert any value from 0 to 999 inclusive, to a string.
+ * @param value The value to convert.
+ * @param and whether to use the word 'and' in the output.
+ * @return a String representation of the value.
+ */
 
-        if (value < SUBTWENTY.length) {
-            return SUBTWENTY[value];
+private static final String tripleAsText(int value, boolean and) {
+    int subhun = value % 100;
+    int hun = value / 100;
+    StringBuilder sb = new StringBuilder(50);
+    if (hun > 0) {
+        sb.append(SUBTWENTY[hun]).append(" hundred ");
+        if (subhun > 0 && and) {
+            sb.append("and ");
         }
-
-        int subhun = value % 100;
-        int hun = value / 100;
-        StringBuilder sb = new StringBuilder(50);
-        if (hun > 0) {
-            sb.append(SUBTWENTY[hun]).append(" hundred");
-        }
-        if (subhun > 0) {
-            if (hun > 0) {
-                sb.append(and ? " and " : " ");
-            }
-            if (subhun < SUBTWENTY.length) {
-                sb.append(SUBTWENTY[subhun]);
-            } else {
-                int tens = subhun / 10;
-                int units = subhun % 10;
-                if (tens > 0) {
-                    sb.append(DECADES[tens]);
-                }
-                if (units > 0) {
-                    sb.append(" ").append(SUBTWENTY[units]);
-                }
-            }
-        }
-
-        return sb.toString();
     }
+    if (subhun < SUBTWENTY.length) {
+        sb.append(SUBTWENTY[subhun]);
+    } else {
+        int tens = subhun / 10;
+        int units = subhun % 10;
+        sb.append(DECADES[tens]);
+        if (units>0) {
+          sb.append(" ");
+        }
+        sb.append(SUBTWENTY[units]);
+    }
+
+    return sb.toString();
+}
 
     /**
      * Convert any long input value to a text representation 

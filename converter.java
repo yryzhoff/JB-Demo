d4a110dcdf2ceef2a2decd75a73742f21f25bbd3
JakeSingerman <jake.singerman@gmail.com>
2015-05-16 17:18:47 +0400

    Improve java converter

--- converter.java
+++ converter.java
@@ -4,7 +4,12 @@ public class IntToText {
     private static final String[] SUBTWENTY = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
                                                "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
     private static final String[] DECADES = {"zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
-
+    /**
+         * Convert any value from 0 to 999 inclusive, to a string.
+         * @param value The value to convert.
+         * @param and whether to use the word 'and' in the output.
+         * @return a String representation of the value.
+         */
     private static final String tripleAsText(int value, boolean and) {
         if (value < 0 || value >= 1000) {
             throw new IllegalArgumentException("Illegal triple-value " + value);
@@ -37,12 +42,11 @@ public class IntToText {
         }
         return sb.toString();
     }
-
     /**
      * Convert any long input value to a text representation 
-     * @param value The value to convert
+     * @param value The value to convert.
      * @param useand true if you want to use the word 'and' in the text (eleven thousand and thirteen)
-     * @param negname
+     
      * @return
      */
     public static final String asText(long value, boolean useand, String negname) {
@@ -56,7 +60,7 @@ public class IntToText {
         // do not make negative numbers positive, to handle Long.MIN_VALUE
         int scale = 0;
         while (value != 0) {
-            // use abs to convert thousand-groups to positive, if needed.
+            // use abs to convert cccc thousand-groups to positive, if needed.
             thous[scale] = Math.abs((int)(value % 1000));
             value /= 1000;
             scale++;
@@ -69,13 +73,12 @@ public class IntToText {
         boolean first = true;
         while (--scale > 0) {
             if (!first) {
-                sb.append(", ");
-            }
+                sb.append(", ")
+           }
             first = false;
             if (thous[scale] > 0) {
                 sb.append(tripleAsText(thous[scale], useand)).append(" ").append(SCALES[scale]);
             }
-
         }
 
         if (!first && useand && thous[0] != 0) {

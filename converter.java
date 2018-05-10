ec85c27e0db2d640f37b210fbc53afd56de27f8d
JakeSingerman <jake.singerman@gmail.com>
2018-05-10 15:52:43 +0300

    Update converter.java

--- converter.java
+++ converter.java
@@ -3,10 +3,11 @@ public class IntToText {
     private static final String[] SCALES = {"", "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion"};
     private static final String[] SUBTWENTY = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
                                                "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
-    private static final String[] DECADES = {"zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+    
     /**
      * Convert any value from 0 to 999 inclusive, to a string.
-     * @param value The value to convert. 
+     * @param value The value to convert.
+     * @param and whether to use the word 'and' in the output.
      */
     private static final String tripleAsText(int value, boolean and) {
         if (value < 0 || value >= 1000) {
@@ -46,9 +47,10 @@ public class IntToText {
 
     /**
      * Convert any long input value to a text representation 
-    
+     * @param value The value to convert
      * @param useand true if you want to use the word 'and' in the text (eleven thousand and thirteen)
-          
+     * @param negname
+     1111
      */
     public static final String asText(long value, boolean useand, String negname) {
         if (value == 0) {
@@ -99,7 +101,9 @@ public class IntToText {
         System.out.printf("%15d %s%n", Long.MAX_VALUE, asText(Long.MAX_VALUE, true, "minus"));
     }
 }
-kkkkkkkkkfff
-oooccccccc
-pppppppp
-00000000
+
+// fix string representation
+kkkkkkkkk
+oooccc
+pppp
+000000

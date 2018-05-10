502239a6ee5ee101c5196ab780b7c9ae7a7b709e
JakeSingerman <jake.singerman@gmail.com>
2018-05-10 15:54:16 +0300

    Update converter

--- converter.java
+++ converter.java
@@ -3,11 +3,13 @@ public class IntToText {
     private static final String[] SCALES = {"", "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion"};
     private static final String[] SUBTWENTY = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
                                                "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
-    
+    private static final String[] DECADES = {"zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+
     /**
      * Convert any value from 0 to 999 inclusive, to a string.
      * @param value The value to convert.
      * @param and whether to use the word 'and' in the output.
+     * @return a String representation of the value.
      */
     private static final String tripleAsText(int value, boolean and) {
         if (value < 0 || value >= 1000) {
@@ -50,7 +52,7 @@ public class IntToText {
      * @param value The value to convert
      * @param useand true if you want to use the word 'and' in the text (eleven thousand and thirteen)
      * @param negname
-     1111
+     * @return
      */
     public static final String asText(long value, boolean useand, String negname) {
         if (value == 0) {
@@ -103,7 +105,8 @@ public class IntToText {
 }
 
 // fix string representation
-kkkkkkkkk
-oooccc
-pppp
-000000
+
+kkkkk9999
+oooc66666
+ppllll
+000011111

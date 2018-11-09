959f603bfdc21d428e3926e40c0ca542559b8912
JakeSingerman <jake.singerman@gmail.com>
2018-11-09 16:49:46 +0300

    Improve converter

--- converter.java
+++ converter.java
@@ -1,17 +1,23 @@
 public class IntToText {
 
+    private static final String[] SCALES = {"", "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion"};
     private static final String[] SUBTWENTY = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
                                                "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
-    private static final String[] DECADES = {"zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+    private static final String[] DECADES = {"ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
 
     /**
      * Convert any value from 0 to 999 inclusive, to a string.
      * @param value The value to convert.
      * @param and whether to use the word 'and' in the output.
+     * @return a String representation of the value.
      */
     private static final String tripleAsText(int value, boolean and) {
         if (value < 0 || value >= 1000) {
             throw new IllegalArgumentException("Illegal triple-value " + value);
+        }
+
+        if (value < SUBTWENTY.length) {
+            return SUBTWENTY[value];
         }
 
         int subhun = value % 100;
@@ -97,5 +103,3 @@ public class IntToText {
         System.out.printf("%15d %s%n", Long.MAX_VALUE, asText(Long.MAX_VALUE, true, "minus"));
     }
 }
-
-
